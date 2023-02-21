b9c40c85fb8032e514315506e49ca11eeb2c519c
Mikasha0 <anikettamrakar0@gmail.com>
2023-02-21 19:27:03 +0545

    Successfully added clear timer

--- portfolio/src/Pages/HookTimer.js
+++ portfolio/src/Pages/HookTimer.js
@@ -1,19 +1,25 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export default function HookTimer() {
   const [timer, setTimer] = useState(0);
+  const intervalRef = useRef();
 
   useEffect(() => {
-    const interval = setInterval(() => {
+    intervalRef.current = setInterval(() => {
       setTimer((value) => value + 1);
     }, 1000);
+    return () => {
+      clearInterval(intervalRef.current);
+    };
   }, []);
 
   return (
     <>
       <div>Start Timer - {timer}</div>
-      <button>Clear Timer</button>
+      <button onClick={() => clearInterval(intervalRef.current)}>
+        Clear Timer
+      </button>
     </>
   );
 }
