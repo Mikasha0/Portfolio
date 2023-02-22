afb612f82425e45f1c2f9b17e321a68eb65fe438
Mikasha0 <anikettamrakar0@gmail.com>
2023-02-22 21:34:13 +0545

    deleted hooktimer.js and made some other changes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
-import HookTimer from "./Pages/HookTimer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <HookTimer />
     </div>
   );
 }
