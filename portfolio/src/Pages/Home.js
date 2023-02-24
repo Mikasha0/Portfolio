bf84f64fcf0d6dca3753a4acebe3a9d5061dac5d
Mikasha0 <anikettamrakar0@gmail.com>
2023-02-24 20:56:23 +0545

    made awesome animation text design

--- portfolio/src/Pages/Home.js
+++ portfolio/src/Pages/Home.js
@@ -1,8 +1,48 @@
 import React from "react";
-import { useState, useEfect } from "react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  useEfect(() => {}, []);
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = [
+    "Full Stack Developer",
+    "Software Developer",
+    "UI/UX Designer",
+  ];
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text, delta]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
   return (
     <section className="home">
       <div className="container">
@@ -10,7 +50,7 @@ export default function Home() {
           <div className="col-12 col-md-6">
             <span className="tagline">Welcome to my Portfolio</span>
             <h1>
-              {`Hi I am Aniket Tamrakar`} <span>Welcome to my Portfolio</span>
+              {`Hi I am Aniket Tamrakar`} <span>{text}</span>
             </h1>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
