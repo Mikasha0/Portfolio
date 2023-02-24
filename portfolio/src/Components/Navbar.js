e28e6352293f5b3d54d7f2cf5912ebf271e3c3d8
Mikasha0 <anikettamrakar0@gmail.com>
2023-02-24 16:52:49 +0545

    completely designed navbar

--- portfolio/src/Components/Navbar.js
+++ portfolio/src/Components/Navbar.js
@@ -3,58 +3,59 @@ import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-dark">
-      <div className="container d-flex justify-content-center">
-        <a className="navbar-brand" href="/">
-          LOGO
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav  mb-2 mb-lg-0 ms-auto">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
-                Skills
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
-                Projects
-              </a>
-            </li>
-          </ul>
-          <div className="social-icons">
-            <FaFacebookF className="icons" />
-            <FaInstagram style={{ color: "white" }} />
-            <FaLinkedinIn style={{ color: "white" }} />
+    <header className="sticky-top">
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-top py-3 ">
+          <a className="navbar-brand" href="index.html">
+            {/* <!-- <img className="logo" src="./img/logo.webp" alt="..." /> --> */}
+            <span className="">Logo</span>
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            style={{ backgroundColor: "white" }}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav gap-lg-4 ms-auto">
+              <li className="nav-item">
+                <a href="/" className="nav-link">
+                  HOME
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link">
+                  SKILLS
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  PROJECTS
+                </a>
+              </li>
+            </ul>
+            <div className="social-links">
+              {" "}
+              <div className="icons-container">
+                <FaFacebookF className="circular-icon" />
+              </div>
+              <div className="icons-container">
+                <FaInstagram className="circular-icon" />
+              </div>
+              <div className="icons-container">
+                <FaLinkedinIn className="circular-icon" />
+              </div>
+            </div>
           </div>
-          {/* <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form> */}
-        </div>
+        </nav>
       </div>
-    </nav>
+    </header>
   );
 }
