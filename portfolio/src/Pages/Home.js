dde0d98355fa6d3c7f795cc9b6d442f58a546da1
Mikasha0 <anikettamrakar0@gmail.com>
2023-02-24 19:39:23 +0545

    added some content on the home page

--- portfolio/src/Pages/Home.js
+++ portfolio/src/Pages/Home.js
@@ -1,5 +1,25 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <section className="home">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-6">
+            <span className="tagline">Welcome to my Portfolio</span>
+            <h1>
+              {`Hi I am Aniket Tamrakar`} <span>Welcome to my Portfolio</span>
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+              nemo nihil molestias, at quos repellat consectetur facilis quasi
+              aliquid eum voluptas fugit explicabo dolorum facere impedit!
+              Eligendi facilis porro voluptas.
+            </p>
+          </div>
+          <div className="col-12 col-md-6"></div>
+        </div>
+      </div>
+    </section>
+  );
 }
