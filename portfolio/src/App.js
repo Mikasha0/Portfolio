cecb6188a542a64148a451b83dce1f7a48f713cf
Mikasha0 <anikettamrakar0@gmail.com>
2023-03-05 10:51:06 +0545

    added a skill page and made some style changes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,12 +4,14 @@ import Home from "./Pages/Home";
 import Footer from "./Components/Footer";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
+import Skills from "./Pages/Skills";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
       <About />
+      <Skills />
       <Contact />
       <Footer />
     </div>
---
+++ portfolio/src/Pages/Skills.js
@@ -0,0 +1,113 @@
+import React from "react";
+
+export default function Skills() {
+  return (
+    <section class="our-services">
+      <div class="container">
+        <div class="section-heading">
+          <span>SERVICES OFFERING</span>
+          <h2>Our Services</h2>
+        </div>
+        <div class="row g-0">
+          <div class="col-12 col-md-6 col-lg-4">
+            <div class="card">
+              <div class="card-body">
+                <div class="icons">
+                  <i class="bi bi-buildings"></i>
+                </div>
+                <h4>Tax Advisory</h4>
+                <p>
+                  Foreign investment in India is primary governed by the FDI
+                  policy formulated by the secreteriat for industrial assistance
+                  (SIA), the Department of industrial policy and promition
+                  (DIPP).
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-4">
+            <div class="card card2">
+              <div class="card-body">
+                <div class="icons">
+                  <i class="bi bi-buildings"></i>
+                </div>
+                <h4>Tax Advisory</h4>
+                <p>
+                  Foreign investment in India is primary governed by the FDI
+                  policy formulated by the secreteriat for industrial assistance
+                  (SIA), the Department of industrial policy and promition
+                  (DIPP).
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-4">
+            <div class="card">
+              <div class="card-body">
+                <div class="icons">
+                  <i class="bi bi-buildings"></i>
+                </div>
+                <h4>Tax Advisory</h4>
+                <p>
+                  Foreign investment in India is primary governed by the FDI
+                  policy formulated by the secreteriat for industrial assistance
+                  (SIA), the Department of industrial policy and promition
+                  (DIPP).
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-12 col-md-6 col-lg-4">
+            <div class="card our-services-border">
+              <div class="card-body">
+                <div class="icons">
+                  <i class="bi bi-buildings"></i>
+                </div>
+                <h4>Tax Advisory</h4>
+                <p>
+                  Foreign investment in India is primary governed by the FDI
+                  policy formulated by the secreteriat for industrial assistance
+                  (SIA), the Department of industrial policy and promition
+                  (DIPP).
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-4">
+            <div class="card card2 our-services-border">
+              <div class="card-body">
+                <div class="icons">
+                  <i class="bi bi-buildings"></i>
+                </div>
+                <h4>Tax Advisory</h4>
+                <p>
+                  Foreign investment in India is primary governed by the FDI
+                  policy formulated by the secreteriat for industrial assistance
+                  (SIA), the Department of industrial policy and promition
+                  (DIPP).
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-4">
+            <div class="card our-services-border">
+              <div class="card-body">
+                <div class="icons">
+                  <i class="bi bi-buildings"></i>
+                </div>
+                <h4>Tax Advisory</h4>
+                <p>
+                  Foreign investment in India is primary governed by the FDI
+                  policy formulated by the secreteriat for industrial assistance
+                  (SIA), the Department of industrial policy and promition
+                  (DIPP).
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
